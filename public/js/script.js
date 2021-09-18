b5572de933358e43ba0cfaf31d957ed64cbba7df
padenaa <padenarasouli@gmail.com>
2021-09-18 22:57:08 +0000

    finding location

--- public/js/script.js
+++ public/js/script.js
@@ -1,2 +1,14 @@
-month = getMonth();
-//check that they entered all the required info 
+d = new Date();
+month = d.getMonth();
+//check that they entered all the required info
+
+
+function printMousePos(e) {
+    var cursorX = e.pageX;
+    var cursorY = e.pageY;
+    console.log(cursorX);
+    console.log(cursorY);
+}
+const location1 = document.getElementById("location");
+location1.addEventListener('click', printMousePos);
+
