fc2a48c9a77dc1867b04a01af6de1fe37e1f60d4
padenaa <padenarasouli@gmail.com>
2021-09-19 03:47:30 +0000

    fixed pin thing

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,6 @@ d = new Date();
 month = d.getMonth();
 //check that they entered all the required info
 
-
 function printMousePos(e) {
     var cursorX = e.clientX;
     var cursorY = e.clientY;
@@ -10,19 +9,37 @@ function printMousePos(e) {
     var country = 0;
     if (cursorY < 390){
         country = 1;
-        el.style.left = 0;
+        y = "80px";
+        x = "130px";
     }else if (cursorY < 450){
         country = 2;
+        y="130px";
+        x= "130px";
     } else {
         country = 3;
+        y="230px";
+        x = "200px";
     }
-    
-    
-    el.style.top = cursorY;
-    el.style.left = cursorX;
+    el.style.top = y;
+    el.style.left = x;
     return country;
 }
 
 const location1 = document.getElementById("location");
 var country = location1.addEventListener('click', printMousePos);
+
+const prompt2a = document.getElementById("breakfast");
+const prompt2b = document.getElementById("lunch");
+const prompt2c = document.getElementById("dinner");
+var meal;
+
+prompt2a.addEventListener('click', ()=>{
+    meal = "breakfast";
+}); 
+prompt2b.addEventListener('click', ()=>{
+    meal = "lunch";
+}); 
+prompt2c.addEventListener('click', ()=>{
+    meal = "dinner";
+}); 
 
