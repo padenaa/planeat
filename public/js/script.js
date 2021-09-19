9dfea35dc71b9f0f03afbbf4a4e16efcae0fc945
padenaa <padenarasouli@gmail.com>
2021-09-19 05:47:23 +0000

    added recipes

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,22 @@
 d = new Date();
 month = d.getMonth();
+season = 0; //1 for spring, 4 for winter
+console.log(month);
+
+if (month<=1){
+    season = 4;
+} else if (month <=4){
+    season = 1;
+} else if (month <= 7){
+    season = 2;
+} else if (month <= 10){
+    season = 3;
+} else{
+    season = 4;
+}
 //check that they entered all the required info
+var entered1 = false;
+var entered2 = false;
 
 function printMousePos(e) {
     var cursorX = e.clientX;
@@ -22,6 +38,7 @@ function printMousePos(e) {
     }
     el.style.top = y;
     el.style.left = x;
+    entered1 = true;
     return country;
 }
 
@@ -31,15 +48,49 @@ var country = location1.addEventListener('click', printMousePos);
 const prompt2a = document.getElementById("breakfast");
 const prompt2b = document.getElementById("lunch");
 const prompt2c = document.getElementById("dinner");
-var meal;
+var meal; 
 
 prompt2a.addEventListener('click', ()=>{
-    meal = "breakfast";
+    meal = 1;
+    entered2 = true;
 }); 
 prompt2b.addEventListener('click', ()=>{
-    meal = "lunch";
+    meal = 2;
+    entered2 = true;
 }); 
 prompt2c.addEventListener('click', ()=>{
-    meal = "dinner";
+    meal = 3;
+    entered2 = true;
 }); 
 
+season = season-1;
+meal = meal-1;
+var recipes_c = [["https://theviewfromgreatisland.com/rhubarb-breakfast-cake-recipe/", "https://smittenkitchen.com/2011/05/ribboned-asparagus-salad-with-lemon/", "https://smittenkitchen.com/2008/09/spinach-quiche-revisited/"],
+                ["https://www.jessicagavin.com/vegan-whole-grain-raspberry-breakfast-bars/","https://smittenkitchen.com/2018/07/grilled-zucchini-ribbons-with-pesto-and-white-beans/","https://www.bettycrocker.com/recipes/stuffed-peppers/63e29e18-903e-467c-aec5-fba4ce3a138f"],
+                ["https://www.delish.com/cooking/recipe-ideas/a28186318/apple-pancakes-recipe/","https://www.loveandlemons.com/butternut-squash-soup/","https://www.delish.com/cooking/recipe-ideas/a28556182/pumpkin-ravioli-recipe/"],
+                ["https://www.tyrantfarms.com/recipe-chestnut-breakfast-porridge/","https://www.bbcgoodfood.com/recipes/turnip-gratin","https://www.bonappetit.com/recipe/sweet-potato-curry"]
+                ];
+var recipes_u = [["https://www.foodnetwork.com/recipes/geoffrey-zakarian/classic-french-toast-with-strawberry-syrup-2314118","https://www.foodnetwork.com/recipes/ina-garten/chive-biscuits-recipe-1947090","https://www.bonappetit.com/recipe/ricotta-gnocchi"],
+                ["https://www.thekitchenmagpie.com/healthy-breakfast-peach-crisp/","https://www.epicurious.com/recipes/food/views/grilled-corn-salad-with-hot-honey-lime-dressing","https://smittenkitchen.com/2014/09/herbed-tomato-and-roasted-garlic-tart/"],
+                ["https://www.chelanfresh.com/crowd-pleasing-pear-bread/","https://www.loveandlemons.com/beet-recipes/","https://www.spendwithpennies.com/scalloped-potatoes-recipe/"],
+                ["https://www.tasteofhome.com/recipes/tangerine-muffins/","https://www.loveandlemons.com/kale-recipes/","https://smittenkitchen.com/2006/10/unflinchingly-good-things/"]
+                ];
+var recipes_m = [["https://www.allrecipes.com/recipe/245303/sweet-coconut-oatmeal/","https://www.delish.com/uk/cooking/recipes/a33009374/scallion-pancakes-recipe/","celery soup"],
+                ["https://somethingnewfordinner.com/recipe/mango-breakfast-jars/","https://www.delish.com/cooking/recipe-ideas/recipes/a51467/easy-homemade-salsa-recipe/","https://www.allrecipes.com/recipe/274814/chicken-tacos-with-pineapple-salsa/"],
+                ["https://www.wellplated.com/watermelon-smoothie/","https://www.bonappetit.com/recipe/cucumber-and-onion-salad","https://www.tasteofhome.com/recipes/southwestern-sauteed-corn/"],
+                ["https://cleangreensimple.com/article/amy-gorin-mandarin-orange-smoothie-bowl-recipe/","https://smittenkitchen.com/2007/06/catch-up-solstice-edition/","https://thenovicechefblog.com/pan-fried-plantains/"]
+                ];
+var link;
+if ((entered1)&&(entered2)){
+    if (country==1){
+        link = recipes_c[season][meal];
+    } else if (country==2){
+        link = recipes_u[season][meal];
+    } else{
+        link = recipes_m[season][meal];
+    }
+}
+
+function recipefinder (){
+    window.location = link;
+}
